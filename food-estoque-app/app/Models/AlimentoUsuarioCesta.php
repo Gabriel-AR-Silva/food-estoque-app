cc26840c30ceafe0d07f5ea545bd5b67a1bc5d6b
Gabriel Augusto Ribeiro da Silva <gabrielsilva.webdeveloper@gmail.com>
2023-06-25 03:11:29 -0300

    iniciando projeto

---
+++ food-estoque-app/app/Models/AlimentoUsuarioCesta.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AlimentoUsuarioCesta extends Model
+{
+    use HasFactory;
+}
---
+++ food-estoque-app/resources/views/food-estoque.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('title', 'Food estoque')
+
+@section('content')
+
+<br> <br> <br>
+    <p>
+        Pg food estoque 
+    </p>
+
+    <a href="/">Voltar a Home</a>
+
+@endsection
